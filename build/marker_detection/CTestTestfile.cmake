281c636fbb087cfa5690d4d222f1c6acb9bf2d97
Gordon Keller <ghkeller@ucsc.edu>
2022-08-02 09:49:45 -0700

    stipulates opencv 3.0.2 to play nice with sim and other components

--- build/marker_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gordon_l1804/detection_ws/src/marker_detection
-# Build directory: /home/gordon_l1804/detection_ws/build/marker_detection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
